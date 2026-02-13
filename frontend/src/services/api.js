c081cf5be4930838bea6c7bd2de42f557223f919
Edward <adjfb.ed@gmail.com>
2026-02-13 19:37:01 -0400

    axios

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333', // La URL por defecto de Adonis
+  baseURL: 'https://programacion-iii-l7b3.onrender.com', // La URL por defecto de Adonis
   headers: {
     'Content-Type': 'application/json'
   }
